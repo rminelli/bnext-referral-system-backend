078adb942adeb48a2adada2c593f7bfee6baf5ab
roberto.minelli <roberto.minelli@ara-tech.es>
2020-10-27 14:54:40 +0100

    FIX: fixed method to use repository instance

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -5,8 +5,6 @@ import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 import { Contact } from './entities/contact.entity';
 import { VerifyPhoneNumber } from '../common/verifyphonenumber.common';
-import { getConnection } from 'typeorm';
-
 @Injectable()
 export class ContactsService {
   constructor(
